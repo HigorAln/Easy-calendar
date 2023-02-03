25a50e47efe8c0bc994404c26204ee79290c32b4
Higor Allan <higor.allan21@gmail.com>
2023-02-02 22:10:53 -0300

    listagem de horas

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -36,7 +36,7 @@ export default async function handle(
   const isPastDate = referenceDate.endOf('day').isBefore(new Date())
 
   if (isPastDate) {
-    return res.json({ availability: [] })
+    return res.json({ possibleTimes: [], availableTimes: [] })
   }
 
   const userAvailability = await prisma.userTimeInterval.findFirst({
@@ -47,7 +47,7 @@ export default async function handle(
   })
 
   if (!userAvailability) {
-    return res.json({ availability: [] })
+    return res.json({ possibleTimes: [], availableTimes: [] })
   }
 
   const { time_end_in_minutes, time_start_in_minutes } = userAvailability
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -4,10 +4,15 @@ import React from 'react'
 import dayjs from 'dayjs'
 import { api } from '@/lib/axios'
 import { useRouter } from 'next/router'
+import { useQuery } from '@tanstack/react-query'
+
+interface Availability {
+  possibleTimes: number[]
+  availableTimes: number[]
+}
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = React.useState<Date | null>(null)
-  const [availability, setAvailability] = React.useState(null)
   const router = useRouter()
 
   const isDateSelected = !!selectedDate
@@ -18,21 +23,25 @@ export function CalendarStep() {
     ? dayjs(selectedDate).format('DD[ de ]MMMM')
     : null
 
-  React.useEffect(() => {
-    if (!selectedDate) {
-      return
-    }
+  const selectedDateWithoutTime = selectedDate
+    ? dayjs(selectedDate).format('YYYY-MM-DD')
+    : null
 
-    api
-      .get(`/users/${username}/availability`, {
+  const { data: availability } = useQuery<Availability>(
+    ['availability', selectedDateWithoutTime],
+    async () => {
+      const response = await api.get(`/users/${username}/availability`, {
         params: {
           date: dayjs(selectedDate).format('YYYY-MM-DD'),
         },
       })
-      .then((response) => {
-        console.log(response.data)
-      })
-  }, [selectedDate, username])
+
+      return response.data
+    },
+    {
+      enabled: !!selectedDate,
+    },
+  )
 
   return (
     <S.Container isTimePickerOpen={isDateSelected}>
@@ -45,19 +54,16 @@ export function CalendarStep() {
           </S.TimePickerHeader>
 
           <S.TimePickerList>
-            <S.TimePickerItem>08:00h</S.TimePickerItem>
-            <S.TimePickerItem>09:00h</S.TimePickerItem>
-            <S.TimePickerItem>10:00h</S.TimePickerItem>
-            <S.TimePickerItem>11:00h</S.TimePickerItem>
-            <S.TimePickerItem>12:00h</S.TimePickerItem>
-            <S.TimePickerItem>13:00h</S.TimePickerItem>
-            <S.TimePickerItem>14:00h</S.TimePickerItem>
-            <S.TimePickerItem>15:00h</S.TimePickerItem>
-            <S.TimePickerItem>16:00h</S.TimePickerItem>
-            <S.TimePickerItem>17:00h</S.TimePickerItem>
-            <S.TimePickerItem>18:00h</S.TimePickerItem>
-            <S.TimePickerItem>19:00h</S.TimePickerItem>
-            <S.TimePickerItem>20:00h</S.TimePickerItem>
+            {availability?.possibleTimes.map((hour) => {
+              return (
+                <S.TimePickerItem
+                  key={hour}
+                  disabled={!availability.availableTimes.includes(hour)}
+                >
+                  {String(hour).padStart(2, '0')}:00h
+                </S.TimePickerItem>
+              )
+            })}
           </S.TimePickerList>
         </S.TimePicker>
       )}
